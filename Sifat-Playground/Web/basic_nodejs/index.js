266c4709a05218ebb2aa5423acf49828d033efb1
SiFAT <faisal.ahmed20@northsouth.edu>
2022-07-09 12:27:16 +0600

    Uploading to the server

--- Sifat-Playground/Web/basic_nodejs/index.js
+++ Sifat-Playground/Web/basic_nodejs/index.js
@@ -45,18 +45,33 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.post("/api/notes", (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-  const note = request.body;
-  note.id = maxId + 1;
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
 
   notes = notes.concat(note);
 
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
