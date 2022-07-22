9423d90908c2d55f17f30d6cc3a614cfbf84daf2
Md Sahadul Hasan Arian <hasan.arian@northsouth.edu>
2022-07-23 03:07:03 +0600

    Testing: Fixed Testing issues

--- backend/controllers/DateTimeController.ts
+++ backend/controllers/DateTimeController.ts
@@ -4,8 +4,8 @@
 const config = require('../utils/config')
 
 // Time Ago Library
-import TimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
+const TimeAgo = require('javascript-time-ago')
+const en = require('javascript-time-ago/locale/en')
 TimeAgo.addDefaultLocale(en)
 const timeAgoLibrary = new TimeAgo('en-US')
 
