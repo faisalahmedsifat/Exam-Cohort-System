021d8e89cd457431b34c8fa8ebee92e3d7eadcaf
Md Sahadul Hasan Arian <hasan.arian@northsouth.edu>
2022-09-07 21:40:12 +0600

    Backend: Fixed MCQ Evaluation on the go

--- backend/controllers/EvaluateAnswer.ts
+++ backend/controllers/EvaluateAnswer.ts
@@ -14,23 +14,44 @@ export class EvaluateAnswer {
   static async evaluateMCQAnswer(answerBody) {
 
   }
-  static async evaluateAnswer(questionID, candidateID){
+  static async evaluateAnswer(questionID, candidateID) {
     let answerBase = await DatabaseController.getAnswerBase(candidateID, questionID)
-    if(answerBase.type === "MICROVIVA"){
+    if (answerBase.type === "MICROVIVA") {
       const questionData = await DatabaseController.getDataAttributesFromInstance(await DatabaseController.getQuestionFromQuestionID(questionID))
       const microQuestionData = await DatabaseController.getDataAttributesFromInstance(await DatabaseController.getMicroVivaQuestionFromQuestionID(questionData.microvivaquestionID))
       let corAnsAudioID = microQuestionData.micCorAudioID
       let answerAudioID = (await DatabaseController.getMicroVivaAnswerFromID(answerBase.microvivaanswerID)).micAnsAudioID
-      let isCorrect = await EvaluateAnswer.evaluateMicroVivaAnswer(corAnsAudioID,answerAudioID)
+      let isCorrect = await EvaluateAnswer.evaluateMicroVivaAnswer(corAnsAudioID, answerAudioID)
       answerBase.isCorrect = isCorrect
-      await answerBase.save()
-      // console.log("isCorrect ", isCorrect);
-    }else if(answerBase.type === "MCQ"){
-      // TODO
-    }
+      if (isCorrect === true) answerBase.scoreRightNow = questionData.marks;
+      else answerBase.scoreRightNow = 0;
+    } else if (answerBase.type === "MCQ") {
+      let correctAnswer = true;
 
+      let question = await DatabaseController.getQuestionFromQuestionID(questionID)
+      let mcqQuestionInstance = await DatabaseController.getMCQQuestionFromQuestionID(question.mcqquestionID)
+      let mcqQuestionOptions = await DatabaseController.getOptionsOfMCQQuestionFromQuestionInstance(mcqQuestionInstance, null, true)
+      let mcqQuestionOptionData = DatabaseController.getDataAttributesFromInstance(mcqQuestionOptions)
+
+      let userResponseID = (await DatabaseController.getMcqAnswerFromID(answerBase.mcqanswerID)).id
+      let userSelectedOptions = DatabaseController.getDataAttributesFromInstance(await DatabaseController.getSelectedOptionsFromMcqAnswerID(userResponseID))
+
+      for (const original of mcqQuestionOptionData) {
+        for (const givenOption of userSelectedOptions) {
+          if(original.id === givenOption.mcqoptionID){
+            correctAnswer &&= (original.isMcqOptionCor === givenOption.isSelectedInAnswer)
+          }
+        }
+      }      
+      answerBase.isCorrect = correctAnswer;
+      if (correctAnswer === true) answerBase.scoreRightNow = question.marks;
+      else answerBase.scoreRightNow = 0;
+    }
+    answerBase.hasAutoEvaluated = true;
+    answerBase.hasAdjustedManually = false;
+    await answerBase.save()
   }
-  static async evaluateMicroVivaAnswer(corAnsAudioID,answerAudioID) {    
+  static async evaluateMicroVivaAnswer(corAnsAudioID, answerAudioID) {
     const audioController = new AudioController(FirebaseStorageSingleton.getInstance());
 
     const corAnsAudioUrl = await audioController.getDownloadUrlFromDetails({
@@ -56,11 +77,12 @@ export class EvaluateAnswer {
 
     const textOfCorAns = await sttHandler.getTextFromAudioUrl(corAnsAudioUrl);
     const textOfGivAns = await sttHandler.getTextFromAudioUrl(answerAudioUrl);
-    const similarities = smHandler.getPercentageSimilarities(textOfCorAns,textOfGivAns);
+    const similarities = smHandler.getPercentageSimilarities(textOfCorAns, textOfGivAns);
 
+    await DatabaseController.setAnsAudioText(answerAudioID, textOfGivAns)
     // console.log("Correct Answer Text: ", textOfCorAns);
     // console.log("Given Answer Text: ", textOfGivAns);
     // console.log("Similarities between texts: ", similarities);
-    return similarities*100 >= MICRO_VIVA_CORRECTNESS_THRESHOLD;
+    return similarities * 100 >= MICRO_VIVA_CORRECTNESS_THRESHOLD;
   }
 }
